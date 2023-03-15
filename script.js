d5655d7047f9179bebc788288914972c9e1dac42
sachinmaurya96 <125910801+sachinmaurya96@users.noreply.github.com>
2023-03-15 13:23:05 +0530

    add working contact form

--- script.js
+++ script.js
@@ -52,44 +52,12 @@ let hours = date.getHours();
 let greating = document.getElementById("greating");
 if (hours >= 5 && hours < 12) {
   greating.innerHTML = "Good Morning";
-  var msg = new SpeechSynthesisUtterance();
-  var voices = window.speechSynthesis.getVoices();
-  msg.voice = voices[1];
-  msg.volume = 1; // From 0 to 1
-  msg.rate = 1; // From 0.1 to 10
-  msg.pitch = 2; // From 0 to 2
-  msg.text = "good morning welcome to my portfolio website";
-  speechSynthesis.speak(msg);
 } else if (hours >= 12 && hours < 18) {
   greating.innerHTML = "Good Afternoon";
-  var msg = new SpeechSynthesisUtterance();
-  var voices = window.speechSynthesis.getVoices();
-  msg.voice = voices[1];
-  msg.volume = 1; // From 0 to 1
-  msg.rate = 1; // From 0.1 to 10
-  msg.pitch = 2; // From 0 to 2
-  msg.text = "good afternoon welcome to my portfolio website";
-  speechSynthesis.speak(msg);
 } else if (hours >= 18 && hours < 20) {
   greating.innerHTML = "Good Evening";
-  var msg = new SpeechSynthesisUtterance();
-  var voices = window.speechSynthesis.getVoices();
-  msg.voice = voices[1];
-  msg.volume = 1; // From 0 to 1
-  msg.rate = 1; // From 0.1 to 10
-  msg.pitch = 2; // From 0 to 2
-  msg.text = "good evening welcome to my portfolio website";
-  speechSynthesis.speak(msg);
 } else {
   greating.innerHTML = "Good Night";
-  var msg = new SpeechSynthesisUtterance();
-  var voices = window.speechSynthesis.getVoices();
-  msg.voice = voices[1];
-  msg.volume = 1; // From 0 to 1
-  msg.rate = 1; // From 0.1 to 10
-  msg.pitch = 2; // From 0 to 2
-  msg.text = "good night welcome to my portfolio website";
-  speechSynthesis.speak(msg);
 }
 
 let typeText = ["Youtuber", "Devloper", "Blogger"];
@@ -102,3 +70,8 @@ setInterval(() => {
   }
   type.innerHTML = typeText[i];
 }, 3000);
+window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
